1af4776abb4042aa4231db7100be7216076b8e09
usdzy <usdzy@student.kit.edu>
2019-11-26 19:20:06 +0100

    modify .genmodel

--- designs/org.palladiosimulator.dataflow.diagram.edit/src-gen/DataDictionary/provider/DataDictionaryItemProvider.java
+++ designs/org.palladiosimulator.dataflow.diagram.edit/src-gen/DataDictionary/provider/DataDictionaryItemProvider.java
@@ -13,14 +13,11 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
-
 import org.palladiosimulator.dataflow.diagram.DataFlowDiagram.provider.DataFlowDiagramEditPlugin;
 
 /**
--- designs/org.palladiosimulator.dataflow.diagram.edit/src-gen/DataDictionary/provider/EntryItemProvider.java
+++ designs/org.palladiosimulator.dataflow.diagram.edit/src-gen/DataDictionary/provider/EntryItemProvider.java
@@ -12,14 +12,11 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
-
 import org.palladiosimulator.dataflow.diagram.DataFlowDiagram.provider.DataFlowDiagramEditPlugin;
 
 /**
