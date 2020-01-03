8e9088cd8f9fb53d9abc979681a4326e674dcd54
usdzy <usdzy@student.kit.edu>
2020-01-03 14:19:48 +0100

    create load resource dialog

--- editors/diagram.editor.sirius/src/diagram/editor/sirius/Services.java
+++ editors/diagram.editor.sirius/src/diagram/editor/sirius/Services.java
@@ -1,17 +1,46 @@
 package diagram.editor.sirius;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.palladiosimulator.dataflow.diagram.DataFlowDiagram.DataFlowDiagram;
+import org.palladiosimulator.dataflow.diagram.DataFlowDiagram.DataFlowDiagramRefinement;
+import org.eclipse.emf.common.ui.dialogs.ResourceDialog;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
     
-    /**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
+    
+    public EObject findSubProcesses(EObject self) {
+    	DataFlowDiagram dfd = (DataFlowDiagram)self;
+       System.out.println(dfd.getRefinedBy()); 
+       return self;
+     }
+     
+    
+    public void loadResources(EObject self) {
+    	System.out.println("LOAD RESOURCE CALLED");
+    	ResourceDialog r = new ResourceDialog(Display.getCurrent().getActiveShell(), "Load DD", SWT.SINGLE);
+    	r.open();
+    
+  
+    	
+    	
+    	
     }
+	/*
+	public List<Process> findSubProcesses(DataFlowDiagramRefinement dfdr) {
+		System.out.println(dfdr);
+		return new ArrayList<Process>();
+	}
+    */
 }
+
+
