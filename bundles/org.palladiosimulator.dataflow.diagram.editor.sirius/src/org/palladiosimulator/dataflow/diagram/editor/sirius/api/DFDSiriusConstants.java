4bfa965f87760fe20d01b89263b5269813b766aa
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-03-25 16:41:14 +0100

    Export constants of editor.

---
+++ bundles/org.palladiosimulator.dataflow.diagram.editor.sirius/src/org/palladiosimulator/dataflow/diagram/editor/sirius/api/DFDSiriusConstants.java
@@ -0,0 +1,15 @@
+package org.palladiosimulator.dataflow.diagram.editor.sirius.api;
+
+import org.eclipse.emf.common.util.URI;
+
+public final class DFDSiriusConstants {
+
+    public static final String VIEWPOINT_NAME = "Dataflows";
+    public static final URI VIEWPOINT_URI = URI
+        .createURI("viewpoint:/org.palladiosimulator.dataflow.diagram.editor.sirius/Dataflows");
+
+    private DFDSiriusConstants() {
+        // intentionally left blank
+    }
+
+}
